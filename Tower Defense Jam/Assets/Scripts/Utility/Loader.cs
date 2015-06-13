23434175d9e18b8c9ee67a875607bdd1a43691cf
Ash Blue <ash@blueashes.com>
2015-06-13 02:36:11 -0700

    Prototype map converted from sketch to 3D

--- Tower Defense Jam/Assets/Scripts/Utility/Loader.cs
+++ Tower Defense Jam/Assets/Scripts/Utility/Loader.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 
 public class Loader : MonoBehaviour {
-	List<GameObject> preload;
+	[SerializeField] List<GameObject> preload;
 
-	// Use this for initialization
 	void Awake () {
 		GameObject shared = new GameObject();
 		shared.name = "_Shared";
 
 		foreach (GameObject go in preload) {
-//			GameObject g = (GameObject)Instantiate(preload);
-//			g.
+			GameObject clone = Instantiate(go) as GameObject;
+			clone.transform.SetParent(shared.transform);
 		}
 	}
 }
---
+++ Tower Defense Jam/Assets/Scripts/Utility/Note.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour {
+	[SerializeField] string author;
+	[TextArea] [SerializeField] string note;
+}
