aaa00918f0d2b764f53adf0dab6ce988196072f6
Ash Blue <ash@blueashes.com>
2015-06-12 21:57:12 -0700

    Added Ash scene and loader

---
+++ Tower Defense Jam/Assets/Scripts/Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Loader : MonoBehaviour {
+	List<GameObject> preload;
+
+	// Use this for initialization
+	void Awake () {
+		GameObject shared = new GameObject();
+		shared.name = "_Shared";
+
+		foreach (GameObject go in preload) {
+//			GameObject g = (GameObject)Instantiate(preload);
+//			g.
+		}
+	}
+}
