52c0ba4a83b6cb13015dbad715cef56c6eb23e95
Gabriel Martinez <gabrielma@google.com>
2015-06-13 15:20:05 -0700

    Actually commit Camera
    Smooth the camera's zoom and add support to move camera by moving the
    mouse to the edge of the screen.

---
+++ Tower Defense Jam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace dt {
+    public class CameraController : MonoBehaviour {
+
+        public float moveSpeed = 1f;
+        public uint moveOffset = 5;
+
+        public float zoomSpeed = 1f;
+        public float minZoom = 4f;
+        public float maxZoom = 20f;
+        public float zoomSmoothTime = 0.3f;
+
+        Camera camera;
+        Vector3 movement;
+        float targetZoom;
+        float zoomVelocity = 0f;
+
+        void Start() {
+            camera = GetComponent<Camera>();
+            targetZoom = camera.orthographicSize;
+        }
+
+        void Update() {
+            // Move the camera with the keyboard.
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+
+            movement.Set(h, 0f, v);
+            movement = movement.normalized * moveSpeed * Time.deltaTime;
+            transform.position += movement;
+
+            // Move the camera with the mouse.
+            movement.Set(0, 0, 0);
+            Vector3 mousePosition = Input.mousePosition;
+            if ((int) mousePosition.x < moveOffset) {
+                movement.x -= 1f;
+            } else if ((int) mousePosition.x > Screen.width - moveOffset) {
+                movement.x += 1f;
+            }
+            if ((int) mousePosition.y < moveOffset) {
+                movement.y -= 1f;
+            } else if ((int) mousePosition.y > Screen.height - moveOffset) {
+                movement.y += 1f;
+            }
+            movement = movement.normalized * moveSpeed * Time.deltaTime;
+            transform.position += movement;
+
+            // Zoom the camera with the mouse wheel.
+            float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+            if (mouseScroll > 0 && targetZoom > minZoom) {
+                targetZoom -= zoomSpeed;
+            } else if (mouseScroll < 0 && targetZoom < maxZoom) {
+                targetZoom += zoomSpeed;
+            }
+            float newZoom = Mathf.SmoothDamp(
+                    camera.orthographicSize, targetZoom, ref zoomVelocity,
+                    zoomSmoothTime);
+            camera.orthographicSize = newZoom;
+        }
+    }
+}
