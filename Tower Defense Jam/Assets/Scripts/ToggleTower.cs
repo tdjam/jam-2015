92b43d6ece80bfc8049b841373fe705ff5572840
Gabriel Martinez <reitaka@gmail.com>
2015-06-13 17:07:55 -0700

    Add ability to toggle tower type

---
+++ Tower Defense Jam/Assets/Scripts/ToggleTower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ToggleTower : MonoBehaviour {
+
+    public GameObject[] towers;
+
+    int currentActive = 0;
+
+    // Use this for initialization
+    void Start() {
+        towers[currentActive].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void OnMouseDown() {
+        towers[currentActive].SetActive(false);
+        currentActive = (currentActive + 1) % towers.Length;
+        towers[currentActive].SetActive(true);
+    }
+}
