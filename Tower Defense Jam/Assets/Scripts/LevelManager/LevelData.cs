30dd71d4e5e0f763d01b3d723d5b6375f0d926f1
Ash Blue <ash@blueashes.com>
2015-06-13 20:29:05 -0700

    Levels can now be assigned via prefabs

--- Tower Defense Jam/Assets/Scripts/LevelManager/LevelData.cs
+++ Tower Defense Jam/Assets/Scripts/LevelManager/LevelData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Level {
 	[System.Serializable]
-	public class LevelData {
+	public class LevelData : MonoBehaviour {
 		[Tooltip("All of these spawners will be made active when the game starts up")]
 		public List<Spawner> spawners;
 	}
