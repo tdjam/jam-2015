9440b5688fda68b4ad2d92e47f81017553f2a9bd
Ash Blue <ash@blueashes.com>
2015-06-14 17:53:12 -0700

    Enemies now die when health reaches 0

---
+++ Tower Defense Jam/Assets/Scripts/Enemy/EnemyDeath.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Combat;
+
+
+namespace Enemy {
+	public class EnemyDeath : MonoBehaviour {
+		Stats stats;
+
+		void Awake () {
+			stats = GetComponent<Stats>();
+		}
+
+		void Update () {
+			if (stats.health.IsDead()) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
