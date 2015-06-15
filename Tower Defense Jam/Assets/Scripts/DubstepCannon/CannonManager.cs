ad5d11cdeb1c2749b4e582ad870c6bdf7dfc6bed
Ash Blue <ash@blueashes.com>
2015-06-14 17:43:44 -0700

    Dubstep cannon bug fixes

--- Tower Defense Jam/Assets/Scripts/DubstepCannon/CannonManager.cs
+++ Tower Defense Jam/Assets/Scripts/DubstepCannon/CannonManager.cs
@@ -6,6 +6,9 @@ namespace Cannon {
 	public class CannonManager : MonoBehaviour {
 		Stats stats;
 
+		[Tooltip("How fast will the charge decrease when firing")]
+		[SerializeField] int chargeDecreaseRate;
+
 		[Tooltip("Allow the cannon to be fired as often as you want?")]
 		[SerializeField] bool debug;
 		
@@ -23,15 +26,31 @@ namespace Cannon {
 			cannonGui.charge.maxValue = stats.charge.ChargeMax;
 			cannonGui.health.maxValue = stats.health.HealthMax;
 		}
+
+		public void FireCannon () {
+			// If fully charged and not firing
+		}
 		
+//		IEnumerator CannonFireLoop () {
+			// turn on the laser
+			// Target the nearest enemy with a relative targeting point
+
+			// Decrase charge at the appropriate rate
+			// When enemy is killed find the next nearest enemy to that targeting point
+			// Repeat until the charge runs out
+//		}
+
+		void Update () {
+			// @TODO Smooth damp these values
+			cannonGui.charge.value = stats.charge.Charge;
+			cannonGui.health.value = stats.health.Health;
+
+			// Enable the fire button if the cannon is fully charged
+		}
+
 		void OnDestroy () {
 			if (cannonGui != null)
 				Destroy(cannonGui.gameObject);
-		}
-
-		void Update () {
-			cannonGui.charge.value = stats.charge.Charge;
-			cannonGui.health.value = stats.health.Health;
 		}
 	}
 }
